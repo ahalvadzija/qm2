380e4213a982779042a1321077637c690dbf7df1
Adnan Halvadžija <36677897+ahalvadzija@users.noreply.github.com>
2025-08-26 22:49:04 +0200

    migration added

--- src/qm2/__main__.py
+++ src/qm2/__main__.py
@@ -1,4 +1,10 @@
 from .app import main
+from qm2.paths import ensure_dirs, migrate_legacy_paths
 
 if __name__ == "__main__":
+    # 🔹 1. Prepare application storage
+    ensure_dirs()
+    migrate_legacy_paths()
+
+    # 🔹 2. Run the app
     main()
